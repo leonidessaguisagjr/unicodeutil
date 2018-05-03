8e173865b200b49fe17544cc69002cbf5dd376c3
Leonides T. Saguisag, Jr <leonidessaguisagjr@yahoo.com>
2018-05-02 18:43:15 -0700

    Add UNIDATA_VERSION to the package.

--- unicodeutil/__init__.py
+++ unicodeutil/__init__.py
@@ -5,4 +5,7 @@ except ImportError:
     from .hangulutil import decompose_hangul_syllable
     from .unicodeutil import CaseFoldingMap, UnicodeData, casefold, preservesurrogates  # Python 3 style import
 
-__all__ = ["CaseFoldingMap", "UnicodeData", "casefold", "decompose_hangul_syllable", "preservesurrogates"]
+UNIDATA_VERSION = "10.0.0"  # Version number of Unicode character data
+
+__all__ = ["CaseFoldingMap", "UnicodeData", "casefold", "decompose_hangul_syllable", "preservesurrogates",
+           "UNIDATA_VERSION"]
