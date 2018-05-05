16fc2115d825d0a823d938bb7e9f43819369a195
Leonides T. Saguisag, Jr <leonidessaguisagjr@yahoo.com>
2018-05-04 23:28:43 -0700

    Initial code for example webapp.

---
+++ examples/webapp/app.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import os.path
+import re
+
+from flask import Flask, abort, jsonify, make_response, redirect, render_template, request, url_for
+
+from unicodeutil import UnicodeData, casefold, UNIDATA_VERSION
+from unicodeutil.unicodeutil import _unichr
+
+app = Flask(__name__)
+app.config["JSON_SORT_KEYS"] = False
+appname = "unicodeutil"
+api_version = "v1.0"
+api_base_url = "/{0}/api/{1}/".format(appname, api_version)
+ucd = UnicodeData()
+
+
+@app.errorhandler(404)
+def handle_404(error):
+    return make_response(jsonify({"error": "URL not found.  Please check your spelling and try again."}), 404)
+
+
+@app.route(api_base_url + "ucd/<lookup>", methods=['GET'])
+def ucd_lookup(lookup):
+    try:
+        lookup_value = int(lookup, 16)
+    except ValueError:
+        abort(404)
+        return
+    char_info = ucd[lookup_value]
+    if not char_info:
+        abort(404)
+    response = char_info._asdict()
+    return jsonify(response)
+
+
+@app.route(api_base_url + "casefold", methods=['POST'])
+def do_casefold():
+    fullcasefold = request.json['fullcasefold'] if 'fullcasefold' in request.get_json() else True
+    useturkicmapping = request.json['useturkicmapping'] if 'useturkicmapping' in request.get_json() else False
+    return jsonify(casefold(request.json['input_string'], fullcasefold=fullcasefold, useturkicmapping=useturkicmapping))
+
+
+@app.route("/")
+def home():
+    return redirect("/unicodeutil/ucd/")
+
+
+@app.route("/unicodeutil/ucd/", methods=['GET', 'POST'])
+def ucd_html():
+    if request.method == "POST":
+        search_type = "value" if "lookup_by_value" in request.form else "name"
+        search_text = request.form.get('search_text')
+        if not search_text:
+            return render_template("ucdlookup_error_template.html", search_text=search_text, search_type=search_type)
+        try:
+            if "lookup_by_value" in request.form:
+                lookup = re.sub(r"^[Uu]\+", "", search_text)  # Strip leading "U+" or "u+" if it is present.
+                ucd[int(lookup, 16)]  # Try doing a lookup so we can trap the KeyError and render the error page.
+            else:
+                lookup = ucd.lookup_by_name(search_text).code[2:]  # Strip the leading "U+"
+        except (KeyError, ValueError):
+            return render_template("ucdlookup_error_template.html", search_text=search_text, search_type=search_type)
+        return redirect("/unicodeutil/ucd/" + lookup)
+    else:
+        return render_template("ucdlookup_template.html")
+
+
+@app.route("/unicodeutil/ucd/<lookup>", methods=['GET'])
+def charinfo_html(lookup):
+    try:
+        lookup_value = int(lookup, 16)
+    except ValueError:
+        abort(404)
+        return
+    char_info = ucd[lookup_value]
+    if not char_info:
+        abort(404)
+    return render_template("charinfo_template.html", char=_unichr(lookup_value), char_info=char_info._asdict(),
+                           ucd_ver=UNIDATA_VERSION)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ examples/webapp/test_app.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import requests
+import unittest
+
+from unicodeutil import UnicodeData, casefold
+
+
+base_url = "http://localhost:5000/unicodeutil/api/v1.0/"
+headers = {"Content-Type": "application/json"}
+ucd = UnicodeData()
+
+
+class TestGetUnicodeData(unittest.TestCase):
+
+    def test_get_name(self):
+        test_value = 0x00DF
+        resp = requests.get(base_url + "ucd/" + "0x%04x" % test_value, headers=headers)
+        self.assertEqual(ucd[test_value].name, resp.json()['name'])
+
+
+class TestCaseFold(unittest.TestCase):
+    leading_trailing_quotes = re.compile(r'^"|"$')
+
+    def test_default_casefold(self):
+        input_string = u"WEIẞ"
+        resp = requests.post(base_url + "casefold", json={"input_string": input_string})
+        resp_str = self.leading_trailing_quotes.sub('', resp.content.strip().decode('unicode-escape'))
+        self.assertEqual(casefold(input_string), resp_str)
+
+    def test_simple_casefold(self):
+        input_string = u"WEIẞ"
+        resp = requests.post(base_url + "casefold", json={"input_string": input_string, "fullcasefold": False})
+        resp_str = self.leading_trailing_quotes.sub('', resp.content.strip().decode('unicode-escape'))
+        self.assertEqual(casefold(input_string, fullcasefold=False), resp_str)
+
+    def test_full_casefold(self):
+        input_string = u"WEIẞ"
+        resp = requests.post(base_url + "casefold", json={"input_string": input_string, "fullcasefold": True})
+        resp_str = self.leading_trailing_quotes.sub('', resp.content.strip().decode('unicode-escape'))
+        self.assertEqual(casefold(input_string, fullcasefold=True), resp_str)
+
+
+if __name__ == "__main__":
+    unittest.main()
