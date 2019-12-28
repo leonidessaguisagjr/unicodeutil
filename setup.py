ee72d16074cd78bd4a0c50d3a7b676e7a07cba5e
Leonides T. Saguisag, Jr <leonidessaguisagjr@yahoo.com>
2019-12-27 23:57:43 -0800

    Fix README on PyPI.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='unicodeutil',
-      version='12.1.dev1',
+      version='12.1.dev2',
       description='Classes and functions for working with Unicode data.',
       long_description=long_description,
       long_description_content_type='text/x-rst',
